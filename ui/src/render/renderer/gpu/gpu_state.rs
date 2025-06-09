9b38ddc457b9d99ef7a566379332843d25818255
teodord25 <djuric.teodor25@gmail.com>
2025-06-09 16:04:41 +0200

    refactor render()

--- ui/src/render/renderer/gpu/gpu_state.rs
+++ ui/src/render/renderer/gpu/gpu_state.rs
@@ -1,6 +1,6 @@
 use glam::{Mat4, Vec3};
 use web_sys::HtmlCanvasElement;
-use wgpu::StoreOp;
+use wgpu::{CommandEncoder, StoreOp, TextureView};
 
 use crate::render::renderer::camera::Camera;
 
@@ -31,6 +31,39 @@ impl GpuState {
         (self.surface_context.config.width as f32, self.surface_context.config.height as f32)
     }
 
+    fn render_pass(&self, encoder: &mut CommandEncoder, view: TextureView) {
+        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::RED),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                view: &self.depth_view,
+                depth_ops: Some(wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(1.0),
+                    store: StoreOp::Store,
+                }),
+                stencil_ops: None,
+            }),
+
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        });
+        rpass.set_pipeline(&self.pipeline);
+        rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));   // mesh verts
+        rpass.set_vertex_buffer(1, self.instance_buffer.slice(..)); // per-instance models
+        rpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+
+        rpass.set_bind_group(0, &self.resource_context.bind_group, &[]);
+
+        rpass.draw_indexed(0..self.num_indices, 0, 0..self.instance_count);
+    }
+
     pub fn render(&mut self) {
         // 1) state already ready
 
@@ -40,38 +73,7 @@ impl GpuState {
 
         // 3) encode a render pass that clears green and draws the quad
         let mut encoder = self.surface_context.device.create_command_encoder(&Default::default());
-        {
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::RED),
-                        store: wgpu::StoreOp::Store,
-                    },
-                })],
-                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                    view: &self.depth_view,
-                    depth_ops: Some(wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(1.0),
-                        store: StoreOp::Store,
-                    }),
-                    stencil_ops: None,
-                }),
-
-                occlusion_query_set: None,
-                timestamp_writes: None,
-            });
-            rpass.set_pipeline(&self.pipeline);
-            rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));   // mesh verts
-            rpass.set_vertex_buffer(1, self.instance_buffer.slice(..)); // per-instance models
-            rpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-
-            rpass.set_bind_group(0, &self.resource_context.bind_group, &[]);
-
-            rpass.draw_indexed(0..self.num_indices, 0, 0..self.instance_count);
-        }
+        self.render_pass(&mut encoder, view);
 
         let yaw = self.camera.yaw;
         let pitch = self.camera.pitch;
